cb28d7720905c1a22d5be5f2a76e8128209b0ded
zilola08 <zmakhmonova@gmail.com>
2022-11-07 22:49:24 +0500

    feat: 04-copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,20 @@
+const fs = require('fs');
+const fsPromises = fs.promises;
+const path = require('path');
+const folderPath = path.join(__dirname,'files');
+
+fsPromises.mkdir(path.join(__dirname,'files-copy'),{ recursive: true })
+  .then(console.log("folder 'files-copy' created"));
+
+fsPromises.readdir(folderPath)
+  .then(items => {
+    items.forEach(item => {
+      const fileDir = path.join(__dirname,'files',item);
+      fsPromises.copyFile(fileDir,path.join(__dirname,'files-copy',item))
+        .then(
+          console.log(`copied ${item} into 'files-copy' folder`)
+        )
+    })
+  })
+
 
